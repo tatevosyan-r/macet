aa194917fedfbd84885c09af62f7e9cee6cfb134
tatevosyan-r <rostisio@yandex.ru>
2025-10-22 19:58:30 +0300

    img

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-// Импорт CSS
+// Импорт CSS файлов
 import './css/reset.css';
 import './css/styleA.css';
-import './css/style.css';
-
-// Импорт Swiper CSS как модуль
 import './swiper/swiper-bundle.min.css';
+import './css/style.css';
 
 // Импорт Swiper JS
 import './swiper/swiper-bundle.min.js';
@@ -12,13 +10,48 @@ import './swiper/swiper-bundle.min.js';
 // Импорт основного скрипта
 import './script.js';
 
-// Импорт всех изображений через контекст
-function importAll(r) {
-  r.keys().forEach(r);
-}
+// Импорт изображений для бургер-меню и header
+import './img/burgerOff.svg';
+import './img/Group.svg';
+import './img/search.svg';
+import './img/burgerOff2.svg';
+import './img/burgerOff3.svg';
+import './img/burger.svg';
+import './img/logo.svg';
+import './img/call.svg';
+import './img/chat.svg';
+import './img/profile.svg';
+import './img/repair.svg';
+import './img/checkstatus.svg';
+import './img/expand.svg';
+import './img/scr.svg';
+import './img/submit.svg';
+import './img/zakaz.svg';
 
-// Изображения
-importAll(require.context('./img/', true, /\.(png|jpg|jpeg|gif|webp|svg)$/));
+// Импорт стрелки
+import './img/strelka.svg';
 
-// Шрифты
-importAll(require.context('./fonts/', true, /\.(woff|woff2|eot|ttf|otf)$/));
+// Импорт основного изображения
+import './img/cps.png';
+
+// Импорт логотипов брендов
+import './img/logoPartn/Lenovo.svg';
+import './img/logoPartn/samsung.svg';
+import './img/logoPartn/apple.svg';
+import './img/logoPartn/viewsonic.svg';
+import './img/logoPartn/bosch.svg';
+import './img/logoPartn/hp.svg';
+import './img/logoPartn/acer.svg';
+import './img/logoPartn/sony.svg';
+import './img/logoPartn/go.svg';
+
+
+import './fonts/TTLakes-Bold.eot';
+import './fonts/TTLakes-Bold.ttf';
+import './fonts/TTLakes-Bold.woff';
+import './fonts/TTLakes-Medium.eot';
+import './fonts/TTLakes-Medium.ttf';
+import './fonts/TTLakes-Medium.woff';
+import './fonts/TTLakes-Regular.eot';
+import './fonts/TTLakes-Regular.ttf';
+import './fonts/TTLakes-Regular.woff';
