c64e688f3a5c274ffc98192abbc6b608653e9be6
tatevosyan-r <rostisio@yandex.ru>
2025-10-22 18:07:09 +0300

    свайпер

--- src/index.js
+++ src/index.js
@@ -1,52 +1,14 @@
-
-
 // Импорт CSS файлов
 import './css/reset.css';
 import './css/styleA.css';
-import './swiper/swiper-bundle.min.css';
 import './css/style.css';
 
-// Импорт Swiper JS
+// Импорт Swiper CSS и JS с правильными путями
+import './swiper/swiper-bundle.min.css';
 import './swiper/swiper-bundle.min.js';
 
 // Импорт основного скрипта
 import './script.js';
-
-// Импорт изображений для бургер-меню и header
-import './img/burgerOff.svg';
-import './img/Group.svg';
-import './img/search.svg';
-import './img/burgerOff2.svg';
-import './img/burgerOff3.svg';
-import './img/burger.svg';
-import './img/logo.svg';
-import './img/call.svg';
-import './img/chat.svg';
-import './img/profile.svg';
-import './img/repair.svg';
-import './img/checkstatus.svg';
-import './img/expand.svg';
-import './img/scr.svg';
-import './img/submit.svg';
-import './img/zakaz.svg';
-
-// Импорт стрелки
-import './img/strelka.svg';
-
-// Импорт основного изображения
-import './img/cps.png';
-
-// Импорт логотипов брендов
-import './img/logoPartn/Lenovo.svg';
-import './img/logoPartn/samsung.svg';
-import './img/logoPartn/apple.svg';
-import './img/logoPartn/viewsonic.svg';
-import './img/logoPartn/bosch.svg';
-import './img/logoPartn/hp.svg';
-import './img/logoPartn/acer.svg';
-import './img/logoPartn/sony.svg';
-import './img/logoPartn/go.svg';
-
 
 // Динамический импорт всех изображений
 const imagesContext = require.context('./img', true, /\.(png|jpg|jpeg|gif|webp|svg)$/);
--- webpack.config.js
+++ webpack.config.js
@@ -1,101 +1,74 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
+module.exports = {
+  entry: './src/index.js',
   
-  // Для GitHub Pages используем относительные пути
-  const isGitHubPages = process.env.NODE_ENV === 'gh-pages' || argv.mode === 'production';
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
+    clean: true,
+    publicPath: './',
+  },
 
-  return {
-    entry: './src/index.js',
+  mode: 'production',
 
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: isProduction ? 'js/[name].[contenthash].js' : 'js/[name].js',
-      clean: true,
-      publicPath: isGitHubPages ? './' : '/',
-    },
-
-    mode: isProduction ? 'production' : 'development',
-
-    devServer: {
-      static: './dist',
-      hot: true,
-      open: true,
-      port: 3001,
-    },
-
-    devtool: isProduction ? 'source-map' : 'eval-source-map',
-
-    module: {
-      rules: [
-        // Обработка HTML
-        {
-          test: /\.html$/i,
-          use: ['html-loader'],
-        },
-
-        // Обработка CSS
-        {
-          test: /\.css$/i,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader'
-          ],
-        },
-
-        // Обработка шрифтов
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'fonts/[name][ext]'
-          }
-        },
-
-        // Обработка изображений
-        {
-          test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'images/[name][ext]'
-          }
-        },
-
-        // Babel для современного JS
-        {
-          test: /\.js$/,
-          exclude: /node_modules|\.min\.js$/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          }
-        }
-      ],
-    },
-
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        minify: isProduction
-      }),
-
-      ...(isProduction ? [
-        new MiniCssExtractPlugin({
-          filename: 'css/[name].[contenthash].css',
-        })
-      ] : []),
-    ],
-
-    optimization: {
-      splitChunks: {
-        chunks: 'all',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ],
       },
-    },
-  };
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]'
+        }
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        { 
+          from: 'src/img', 
+          to: 'images',
+          noErrorOnMissing: true
+        },
+        { 
+          from: 'src/fonts', 
+          to: 'fonts',
+          noErrorOnMissing: true 
+        },
+        { 
+          from: 'src/swiper', 
+          to: 'swiper',
+          noErrorOnMissing: true 
+        },
+      ],
+    }),
+  ],
 };
